13bb3eb5388ca331296f9e3293556fc90dc5f468
aztar-dev <ts2.sergent@gmail.com>
2024-06-11 17:47:40 +0200

    COMMIT FINAL (normalement)

--- api.js
+++ api.js
@@ -9,47 +9,29 @@ const getRandomJoke = async () => {
 	}
 };
 
-// const getUsers = async () => {
-// 	try {
-// 		const { data } = await api.get("users");
-// 		return data;
-// 	} catch (error) {
-// 		console.error("Une erreur s'est produite : ", error);
-// 	}
-// };
+const getJoke = async (idToSearch) => {
+	try {
+		const { data } = await api.get(`${idToSearch}`);
+		return data;
+	} catch (error) {
+		console.error("Une erreur s'est produite : ", error);
+	}
+};
 
-// const getUser = async (id) => {
-// 	try {
-// 		const { data } = await api.get(`users/${id}`);
-// 		return data;
-// 	} catch (error) {
-// 		console.error("Une erreur s'est produite : ", error);
-// 	}
-// };
+const getCategories = async () => {
+	try {
+		const { data } = await api.get(`categories`);
+		return data;
+	} catch (error) {
+		console.error("Une erreur s'est produite : ", error);
+	}
+};
 
-// const createUser = async (user) => {
-// 	try {
-// 		const { data } = await api.post("users", user);
-// 		return data;
-// 	} catch (error) {
-// 		console.error("Une erreur s'est produite : ", error);
-// 	}
-// };
-
-// const updateUser = async (user) => {
-// 	try {
-// 		const { data } = await api.put(`users/${user.id}`, user);
-// 		return data;
-// 	} catch (error) {
-// 		console.error("Une erreur s'est produite : ", error);
-// 	}
-// };
-
-// const deleteUser = async (id) => {
-// 	try {
-// 		const { data } = await api.delete(`users/${id}`);
-// 		return data;
-// 	} catch (error) {
-// 		console.error("Une erreur s'est produite : ", error);
-// 	}
-// };
+const getJokeByCategory = async (category) => {
+	try {
+		const { data } = await api.get(`random?category=${category}`);
+		return data;
+	} catch (error) {
+		console.error("Une erreur s'est produite : ", error);
+	}
+};
--- script.js
+++ script.js
@@ -1,18 +1,65 @@
 const jokeParagraph = document.querySelector(".joke-text");
 const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+const searchBtn = document.querySelector(".search-btn");
+const categoriesMenu = document.querySelector("#categories");
+let jokeIndex = 0;
+let jokes = [];
 let jokeID;
 
 const init = async () => {
-	await displayJoke();
+	prevBtn.setAttribute("disabled", "");
+	await displayRandomJoke();
+	await setCategories();
 };
 
-const displayJoke = async () => {
+const displayRandomJoke = async () => {
 	try {
 		let newJoke = await getRandomJoke();
-		jokeID = newJoke.id;
 		jokeParagraph.textContent = newJoke.value;
+		const newJokeDetails = {
+			id: newJoke.id,
+			url: newJoke.url,
+			value: newJoke.value,
+		};
 
-		console.log(newJoke);
+		jokes.push(newJokeDetails);
+		localStorage.setItem("jokes", JSON.stringify(jokes));
+
+		return newJoke;
+	} catch (error) {
+		console.error("Une erreur s'est produite : ", error);
+	}
+};
+
+const displayJokeByCategory = async (category) => {
+	try {
+		let newJoke = await getJokeByCategory(category);
+		jokeParagraph.textContent = newJoke.value;
+		const newJokeDetails = {
+			id: newJoke.id,
+			url: newJoke.url,
+			value: newJoke.value,
+		};
+
+		jokes.push(newJokeDetails);
+		localStorage.setItem("jokes", JSON.stringify(jokes));
+
+		return newJoke;
+	} catch (error) {
+		console.error("Une erreur s'est produite : ", error);
+	}
+};
+
+const setCategories = async () => {
+	try {
+		let categories = await getCategories();
+
+		categories.forEach((category) => {
+			const newOption = document.createElement("option");
+			newOption.textContent = category;
+			categoriesMenu.appendChild(newOption);
+		});
 	} catch (error) {
 		console.error("Une erreur s'est produite : ", error);
 	}
@@ -20,6 +67,52 @@ const displayJoke = async () => {
 
 init();
 
-nextBtn.addEventListener("click", () => {
-	console.log(jokeID);
+//
+//
+// EVENT LISTENERS
+
+nextBtn.addEventListener("click", async () => {
+	if (jokeIndex === 0) {
+		prevBtn.removeAttribute("disabled", "");
+	}
+
+	if (jokeIndex === jokes.length - 1) {
+		// Générer une nouvelle blague (on est revenus à la dernière blague générée)
+		await displayRandomJoke();
+		jokeIndex += 1;
+	} else {
+		// Reparcourir les blagues déjà générées
+		jokeIndex += 1;
+		const viewJoke = await getJoke(jokes[jokeIndex].id);
+		jokeParagraph.textContent = viewJoke.value;
+	}
+});
+
+prevBtn.addEventListener("click", async () => {
+	if (jokeIndex > 0) {
+		jokeIndex -= 1;
+		const viewJoke = await getJoke(jokes[jokeIndex].id);
+		jokeParagraph.textContent = viewJoke.value;
+	}
+
+	if (jokeIndex === 0) {
+		prevBtn.setAttribute("disabled", "");
+	}
+});
+
+searchBtn.addEventListener("click", async () => {
+	if (jokeIndex === 0) {
+		prevBtn.removeAttribute("disabled", "");
+	}
+
+	if (jokeIndex !== jokes.length - 1) {
+		// Nous replace à la fin du tableau pour regénérer une nouvelle blague
+		jokeIndex = jokes.length - 1;
+		await displayJokeByCategory(categoriesMenu.value);
+		jokeIndex += 1;
+	} else {
+		// On est déjà à la fin du tableau
+		await displayJokeByCategory(categoriesMenu.value);
+		jokeIndex += 1;
+	}
 });
